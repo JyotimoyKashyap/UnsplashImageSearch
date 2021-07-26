ad8550cbb2b511f4cf0570fee6c20d075b7953a7
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-07-26 15:08:41 +0530

    - set up app module object for dagger hilt

---
+++ app/src/main/java/com/jyotimoykashyap/unsplashimagesearch/dependencyinjection/AppModule.kt
@@ -0,0 +1,31 @@
+package com.jyotimoykashyap.unsplashimagesearch.dependencyinjection
+
+import com.jyotimoykashyap.unsplashimagesearch.api.UnsplashApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+   // Here we put two methods which knows how to make the objects for us
+    // which is provided by dagger hilt
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(UnsplashApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideUnsplashApi(retrofit: Retrofit): UnsplashApi =
+        retrofit.create(UnsplashApi::class.java)
+
+}
